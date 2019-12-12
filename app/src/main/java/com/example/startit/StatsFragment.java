bd634a9ea1553a5ea6d31278bc8b161899e2cc2c
mdechdee <dechdee.m@gmail.com>
2019-12-12 21:36:34 +0900

    Analytics fragment draft

--- app/src/main/java/com/example/startit/StatsFragment.java
+++ app/src/main/java/com/example/startit/StatsFragment.java
@@ -1,5 +1,7 @@
 package com.example.startit;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,10 +10,58 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_stats, container, false);
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        LineChart chart = (LineChart) getActivity().findViewById(R.id.chart);
+        List<Entry> entries = new ArrayList<Entry>();
+        for (int i = 0 ; i<= 6 ; i ++) {
+            // turn your data into Entry objects
+            entries.add(new Entry(i, i*i));
+        }
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        dataSet.setColor(R.color.colorPrimaryWhite);
+        dataSet.setFillColor(R.color.colorPrimaryWhite);
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSet.setFillAlpha(255);
+        dataSet.setCubicIntensity(0.2f);
+        dataSet.setDrawFilled(true);
+        dataSet.setLineWidth(3.0f);
+        dataSet.setDrawCircles(false);
+
+        YAxis y = chart.getAxisLeft();
+        y.setLabelCount(6, false);
+        y.setTextColor(Color.WHITE);
+        y.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+        y.setDrawGridLines(false);
+        y.setAxisLineColor(Color.WHITE);
+
+        XAxis x = chart.getXAxis();
+        x.setLabelCount(6, false);
+        x.setTextColor(Color.WHITE);
+        x.setPosition(XAxis.XAxisPosition.BOTTOM);
+        x.setDrawGridLines(false);
+        x.setAxisLineColor(Color.WHITE);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.setDrawGridBackground(false);
+        chart.invalidate(); // refresh
     }
 }
