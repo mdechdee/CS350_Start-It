4a91f1b797923b1c751a022213f928f63af52cf5
mdechdee <dechdee.m@gmail.com>
2019-12-12 18:12:13 +0900

    Input can convert to todo item

--- app/src/main/java/com/example/startit/AddItemFragment.java
+++ app/src/main/java/com/example/startit/AddItemFragment.java
@@ -23,13 +23,19 @@ import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 
 import static java.time.LocalDateTime.*;
 
 public class AddItemFragment extends Fragment {
     private int number = 0;
-
+    private final ArrayList<String> tagsAdded = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_item, container, false);
@@ -38,13 +44,12 @@ public class AddItemFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
-
+        //Setup spinner for importance picking
         Spinner spinner = view.findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.importance_level, R.layout.custom_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapter);
-        spinner.setOnItemSelectedListener(new SpinnerActivity());
         //Setup onclick listener for date picker button on the layout
         Button datePickerButton = view.findViewById(R.id.datePickerButton);
         datePickerButton.setOnClickListener(new View.OnClickListener() {
@@ -92,12 +97,14 @@ public class AddItemFragment extends Fragment {
                 chip.setChipIconTintResource(R.color.chipIconTint);
                 chip.setClickable(false);
                 chip.setCheckable(false);
+                tagsAdded.add(txt);
 
                 chipGroup.addView(chip);
                 chip.setOnCloseIconClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        chipGroup.removeView(chip);
+                    chipGroup.removeView(chip);
+                    tagsAdded.remove(chip.getText().toString());
                     }
                 });
             }
@@ -122,23 +129,41 @@ public class AddItemFragment extends Fragment {
     public void onCancle(View view){
         getFragmentManager().popBackStack();
     }
-    public void onConfirm(View view){
-        TextInputEditText inputTitle = getView().findViewById(R.id.titleName);
 
-        ToDoItem toDoItem = new ToDoItem(inputTitle.getText().toString(), now(), now() , Duration.ZERO, 1);
+    public void onConfirm(View view){
+        String todoName;
+        TextInputEditText inputTitle = getView().findViewById(R.id.titleName);
+        todoName = inputTitle.getText().toString();
+
+        LocalDateTime deadline = now();
+        TextInputEditText inputDeadline = getView().findViewById(R.id.inputDeadline);
+        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            Date date = format.parse(inputDeadline.getText().toString());
+            deadline = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        int difficulty = 0;
+        Spinner inputImportance = getView().findViewById(R.id.spinner);
+        String importanceString = inputImportance.getSelectedItem().toString();
+        switch(importanceString){
+            case "Highest":
+                difficulty = 5; break;
+            case "High":
+                difficulty = 4; break;
+            case "Medium":
+                difficulty = 3; break;
+            case "Low":
+                difficulty = 2; break;
+            case "Lowest":
+                difficulty = 1; break;
+        }
+
+        ToDoItem toDoItem = new ToDoItem(todoName,"", tagsAdded, now() ,deadline,  Duration.ZERO, difficulty);
         ToDoFragment.addToTodoList(toDoItem);
         getFragmentManager().popBackStack();
-    }
-
-    private class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
-        public void onItemSelected(AdapterView<?> parent, View view,
-                                   int pos, long id) {
-            Log.d("D", Integer.toString(pos));
-        }
-
-        public void onNothingSelected(AdapterView<?> parent) {
-
-        }
     }
 
 }
